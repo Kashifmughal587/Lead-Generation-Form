d62191aa1f95a319dda6021981e85c27738fcdfc
Kashif Shahzad <kashifmughal587@gmail.com>
2024-01-30 00:33:43 +0500

    First Commit Till Page 13

---
+++ assets/js/custom.js
@@ -0,0 +1,8 @@
+document.getElementById('clearEstimatedHomeValue').addEventListener('click', function() {
+    document.getElementById('estimatedHomeValue').value = '';
+});
+
+document.getElementById('clearEstimatedLoanValue').addEventListener('click', function() {
+    document.getElementById('estimatedLoanValue').value = '';
+});
+
